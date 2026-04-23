638265f22fe7cd321ed0caf789c5fcb0411bd8da
Rizaldi02 <mrizaldiwahyu@gmail.com>
2026-04-23 13:39:19 +0700

    tambahan

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
-    return view('ini adalah halaman awal');
+    return view('home');
 });
 Route::get('/coba_dulu', function () {
-    return view('bismillah');
+    return view('coba_dulu');
 });
