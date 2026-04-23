e2140aae333bb88c1151d11abf86816bbd353a3b
Rizaldi02 <mrizaldiwahyu@gmail.com>
2026-04-23 13:46:55 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,13 @@
- <h1> halaman home </h1>
-ini adalah halaman awal
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+     <h1> halaman home </h1> <br>
+     <h2>ini adalah halaman awal</h2>
+</body>
+</html>
