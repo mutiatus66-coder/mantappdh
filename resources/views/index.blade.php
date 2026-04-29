f10976d5f20a3b510e040d97edf08267c9ce84c2
Regan <regandewad@gmail.com>
2026-04-29 09:59:39 +0700

    vfwea

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/scss')
+    @vite('../resources/scss/index.scss')
     <title>Dashboard · 2026 Redesign Preview</title>
     <script>
       (function () {
