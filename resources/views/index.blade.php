8dfc9be3100a346f192ec9e302e7d27554dbc829
Regan <regandewad@gmail.com>
2026-05-07 10:22:10 +0700

    Triple T

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -525,10 +525,10 @@
             </div>
 
             <!-- Header right: theme mode + user menu -->
-            // <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
-            //   <div class="d-flex align-items-stretch" id="kt_header_nav"></div>
-            //     <p class="mb-0 flex-grow-1 header-welcome-text">.....    &nbsp;</p>
-            //   <div class="d-flex align-items-stretch flex-shrink-0">
+            <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+              <div class="d-flex align-items-stretch" id="kt_header_nav"></div>
+                <p class="mb-0 flex-grow-1 header-welcome-text">.....    &nbsp;</p>
+                <div class="d-flex align-items-stretch flex-shrink-0">
 
                 <!-- Theme Mode Toggle -->
                 <div class="d-flex align-items-center ms-1 ms-lg-3">
