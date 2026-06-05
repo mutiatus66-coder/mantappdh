7de07c4ab91a6c88f4349c0025b39e5f50365984
Regan <regandewad@gmail.com>
2026-06-05 09:00:22 +0700

    Penyesuaian MiddleWare

--- app/Http/Middleware/AksesMiddleware.php
+++ app/Http/Middleware/AksesMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoleMiddleware
+class AksesMiddleware
 {
     public function handle(Request $request, Closure $next, string ...$roles): Response
     {
